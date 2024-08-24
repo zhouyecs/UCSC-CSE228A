38f7fa865477627058aa53b0a87937da00aa90e1
Zhou Ye <zhouye@stu.pku.edu.cn>
2024-08-25 02:21:51 +0800

    finish hw5

--- hw/hw5/src/main/scala/hw5/Cache.scala
+++ hw/hw5/src/main/scala/hw5/Cache.scala
@@ -70,5 +70,83 @@ class Cache(val p: CacheParams) extends Module {
 class DMCache(p: CacheParams) extends Cache(p) {
 	require(p.associativity == 1)
   // BEGIN SOLUTION
-  ???
+  val tags   = SyncReadMem(p.numSets, UInt(p.numTagBits.W))
+	val valids = RegInit(VecInit(Seq.fill(p.numSets)(false.B)))
+	val data   = SyncReadMem(p.numSets, CacheBlock())
+
+	val state  = RegInit(0.U(2.W)) // 0: ready, 1: lookup, 2: fetch
+
+	io.in.ready  := true.B
+	io.hit       := false.B
+	io.out.valid := false.B
+	io.out.bits  := 0.U
+
+	extMem.io.rAddr := 0.U
+	extMem.io.rEn   := false.B
+	extMem.io.wAddr := 0.U
+	extMem.io.wEn   := false.B
+	extMem.io.wData := VecInit(Seq.fill(p.blockSize)(0.U))
+
+	val memReadWire  = Wire(CacheBlock())
+	val dataReadWire = Wire(CacheBlock())
+	val tagReadWire  = Wire(UInt(p.numTagBits.W))
+
+	memReadWire  := extMem.io.rData
+	dataReadWire := data.read(index, io.in.fire)
+	tagReadWire  := tags.read(index, io.in.fire)
+
+	when (state === 0.U) {
+		io.in.ready   := true.B
+		io.hit        := false.B
+		io.out.valid  := false.B
+		extMem.io.wEn := false.B
+		extMem.io.rEn := false.B
+
+		when (io.in.fire) {
+			state := 1.U
+		}
+	} .elsewhen (state === 1.U) {
+		io.in.ready   := false.B
+
+		when (valids(index) && tag === tagReadWire) {
+			io.hit := true.B
+			
+			when (io.in.bits.write) {
+				dataReadWire(offset) := io.in.bits.wData
+				data.write(index, dataReadWire)
+			} .otherwise {
+				io.out.bits := dataReadWire(offset)
+			}
+
+			io.out.valid := true.B
+			state := 0.U
+		} .otherwise {
+			io.hit          := false.B
+			extMem.io.rAddr := io.in.bits.addr / p.blockSize.U
+			extMem.io.rEn   := true.B
+			state           := 2.U
+		}
+	} .elsewhen (state === 2.U) {
+		io.hit        := false.B
+		io.in.ready   := false.B
+		extMem.io.rEn := false.B
+
+		when (valids(index)) {
+			extMem.io.wAddr := (tagReadWire << p.numIndexBits.U) | index
+			extMem.io.wEn   := true.B
+			extMem.io.wData := dataReadWire
+		}
+
+		when (io.in.bits.write) {
+			memReadWire(offset) := io.in.bits.wData
+		} .otherwise {
+			io.out.bits := memReadWire(offset)
+		}
+
+		data.write(index, memReadWire)
+		tags.write(index, tag)
+		valids(index) := true.B
+		io.out.valid := true.B
+		state := 0.U
+	}
 }
